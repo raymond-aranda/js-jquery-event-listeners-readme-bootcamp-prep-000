adcee18232defec81117cdb32b6c0808fed68cf6
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-27 01:10:10 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,10 @@
 //define functions here
-$(document).ready(function(){
+function getIt() {
+  $('p').on(click, function() {
+    alert("Hey!");
+  })
+}
 
-  function getIt() {
-    $('p').on(click, function() {
-      alert("Hey!");
-    })
-  }
-});
 
 $(document).ready(function(){
 
